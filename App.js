dae982af61bab97c6ab842b9ed1487d11a5a034f
Isabel Tripp <stagestruck1@gmail.com>
2020-02-20 01:08:46 -0500

    required app functionality

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /**
- * Second Nature Coding Challenge
+ * Second Nature React Native Challenge
  * Isabel Tripp
  *
  * Requirements:
@@ -21,24 +21,99 @@ import {
   View,
   Text,
   StatusBar,
+  Image,
+  TextInput,
 } from 'react-native';
 
 import Colors from './Colors';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+
+    this.updateName = this.updateName.bind(this);
+  }
+
+  updateName = text => {
+    this.setState({
+      name: text,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.sectionContainer}>
+              <Image
+                source={require('./assets/sn-logo-black-side.png')}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.textSection}>
+                  Hello, my name is: {this.state.name}
+                </Text>
+              </View>
+              <View style={styles.inputSection}>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={text => this.updateName(text)}
+                  value={this.state.name}
+                  maxLength={50}
+                  selectionColor={Colors.red}
+                />
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
 };
 
-const styles = StyleSheet.create({});
-
-export default App;
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.white,
+  },
+  input: {
+    paddingTop: 10,
+    paddingRight: 15,
+    fontSize: 34,
+    color: Colors.dark,
+    fontWeight: '500',
+    marginLeft: '5%',
+  },
+  body: {
+    backgroundColor: Colors.blue,
+  },
+  inputSection: {
+    backgroundColor: Colors.lighter,
+    marginRight: '10%',
+    marginLeft: '10%',
+    marginBottom: 30,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    marginBottom: 24,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+  },
+  textSection: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.lighter,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    backgroundColor: Colors.purple,
+  },
+});
