54103339624bc10cfc0c6cb85e58ea5649c7a128
Isabel Tripp <stagestruck1@gmail.com>
2020-02-20 00:58:34 -0500

    remove NewAppScreen code

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Second Nature Coding Challenge
+ * Isabel Tripp
+ *
+ * Requirements:
+ * 1. The app should display the Second Nature logo at the top
+ * 2. The app should have text underneath the logo reading 'Hello, my name is '
+ * 3. The app should have a text input underneath the 'Hello, my name is '
+ *    which when text is entered, appends text to the end of 'Hello, my name is '.
  *
  * @format
  * @flow
  */
 
 import React from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,13 +23,7 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Colors from './Colors';
 
 const App: () => React$Node = () => {
   return (
@@ -32,83 +33,12 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
         </ScrollView>
       </SafeAreaView>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ Colors.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default {
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  dark: '#444',
+  black: '#000',
+  blue: '#00B9CD', // Second Nature blue
+  red: '#FA4F26', // Second Nature red
+  purple: '#aa00ff', // Second Nature purple (appx.)
+};
